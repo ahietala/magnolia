b6baa0c501c613f511fc6ae3fc5b499584b7469a
ahietala <antti.hietala@magnolia-cms.com>
2017-07-14 17:02:44 +0200

    Check URL for app prefixes

--- jump-to-author/jumpToAuthor.user.js
+++ jump-to-author/jumpToAuthor.user.js
@@ -15,7 +15,14 @@
     var defaultExtension = "html";
     var handlePrefix = "/travel";
 
-    // Get path before selectors and extension.
+    // When content is managed in an app list the URL prefixes and the corresponding app names here.
+    var apps = {
+      "/tours":"tours",
+      "/john":"guitar",
+      "/ringo":"drums",
+    };
+
+    // Remove selectors and extension from the path.
     var path = window.location.pathname;
     console.log("full path: " + path);
 
@@ -27,6 +34,38 @@
       path = path.replace(/\.[^/.]+$/, "");
       console.log("path without extension: " + path);
     }
+
+    // Get first segment of path.
+    var pathSegments = path.split("/");
+    console.log("Splitted segments: " + pathSegments);
+
+    var firstSegment = "/" + pathSegments[1];
+    console.log("First segment: " + firstSegment);
+
+    var shiftedSegments = pathSegments.shift();
+    console.log("Shifted segments: " + shiftedSegments);
+
+
+    // Check if first path segment is a known app prefix.
+    if (firstSegment in apps) {
+      console.log("yes, it's in there");
+      console.log("App name: " + apps[firstPathSegment]);
+
+      console.log("App URL: " + defaultBaseURL + "/.magnolia/admincentral#app:" + apps[firstSegment] + ":detail;" + path + ":edit");
+    } else {
+      console.log("Page URL: " + defaultBaseURL + "/.magnolia/admincentral#app:pages:detail;" + path + ":edit");
+    }
+    /* Object.keys(apps).forEach(function(key) {
+      if (apps[key] == firstPathSegment) {
+        console.log("This content is managed in an app!");
+        console.log("App URL: " + defaultBaseURL + "/.magnolia/admincentral#app:" + apps.key + ":detail;" + path + ":edit");
+      } else {
+        // Append handlePrefix
+        path = handlePrefix + path;
+        console.log("Page URL: " + defaultBaseURL + "/.magnolia/admincentral#app:pages:detail;" + path + ":edit");
+      }
+    }); */
+
 
 
 
