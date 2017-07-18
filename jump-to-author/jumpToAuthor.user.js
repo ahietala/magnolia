2745ccecae79a09df6c5122906fa4e214d4ef610
ahietala <antti.hietala@magnolia-cms.com>
2017-07-18 22:31:51 +0200

    Converter user script to Chrome Extension

--- jump-to-author/jumpToAuthor.user.js
+++ jump-to-author/jumpToAuthor.user.js
@@ -2,72 +2,61 @@
 // @name          Jump to Magnolia author
 // @namespace     https://github.com/ahietala/magnolia
 // @version       0.1
-// @description	  Magnolia typically comes with two instances: author and public. This script allows you to move quickly from the public website to the corresponding content item on the author instance, for example to fix a typo.
+// @description	  Move quickly from a public Magnolia instance to the author instance to edit the content.
 // @author        Antti Hietala
 // @match         https://demopublic.magnolia-cms.com/*
 // @grant         none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    // Author instance and site configuration
-    var defaultBaseURL = "http://demoauthor.magnolia-cms.com";
-    var defaultExtension = "html";
-    var handlePrefix = "/travel";
 
-    // When content is managed in an app list the URL prefixes and the corresponding app names here.
-    var apps = {
-      "/tours":"tours",
-      "/john":"guitar",
-      "/ringo":"drums",
-    };
+function editInMagnolia() {
 
-    // Remove selectors and extension from the path.
-    var path = window.location.pathname;
-    console.log("full path: " + path);
+  'use strict';
+  // Author instance and site configuration
+  var defaultBaseURL = "http://demoauthor.magnolia-cms.com";
+  var defaultExtension = "html";
+  var handlePrefix = "/travel";
 
-    if (path.includes("~")) {
-      path = path.replace(/~.*/, "");
-      console.log("path without selectors: " + path);
-    }
-    else {
-      path = path.replace(/\.[^/.]+$/, "");
-      console.log("path without extension: " + path);
-    }
+  // URL prefixes that suggest the content is managed in an app
+  // "<URL prefix>":"appName"
+  var apps = {
+    "/tours":"tours",
+    "/john":"guitar",
+    "/ringo":"drums",
+  };
+  console.log(apps);
+  // Remove selectors and extension from the path.
+  var path = window.location.pathname;
+  console.log("full path: " + path);
 
-    // Check if first path segment is a known app prefix.
+  if (path.includes("~")) {
+    path = path.replace(/~.*/, "");
+    console.log("path without selectors: " + path);
+  }
+  else {
+    path = path.replace(/\.[^/.]+$/, "");
+    console.log("path without extension: " + path);
+  }
 
-    var pathSegments = path.split("/");
-    console.log("Path segments: " + pathSegments);
+  // Check if first path segment is a known app prefix.
 
-    var firstSegment = "/" + pathSegments[1];
-    console.log("First segment: " + firstSegment);
+  var segments = path.split("/");
+  console.log(segments);
 
-    if ("/" + pathSegments[1] in apps) {
-      console.log("This content is managed in the " + apps[firstSegment] + " app.");
-      pathSegments.shift();
-      console.log("App URL: " + defaultBaseURL + "/.magnolia/admincentral#app:" + apps[firstSegment] + ":detail;" + pathSegments + ":edit");
-    } else {
-      console.log("This content is managed in the Pages app.");
-      console.log("Page URL: " + defaultBaseURL + "/.magnolia/admincentral#app:pages:detail;" + handlePrefix + path + ":edit");
-    }
-    /* Object.keys(apps).forEach(function(key) {
-      if (apps[key] == firstPathSegment) {
-        console.log("This content is managed in an app!");
-        console.log("App URL: " + defaultBaseURL + "/.magnolia/admincentral#app:" + apps.key + ":detail;" + path + ":edit");
-      } else {
-        // Append handlePrefix
-        path = handlePrefix + path;
-        console.log("Page URL: " + defaultBaseURL + "/.magnolia/admincentral#app:pages:detail;" + path + ":edit");
-      }
-    }); */
+  var firstSegment = "/" + segments[1];
+  console.log(firstSegment);
 
+  if (firstSegment in apps) {
+    var appName = apps[firstSegment];
+    segments.splice(1,1);
+    var pathInApp = segments.join("/");
+    console.log("This content is managed in the " + appName + " app.");
+    console.log("App URL: " + defaultBaseURL + "/.magnolia/admincentral#app:" + appName + ":detail;" + pathInApp + ":edit");
+  } else {
+    console.log("This content is managed in the Pages app.");
+    console.log("Page URL: " + defaultBaseURL + "/.magnolia/admincentral#app:pages:detail;" + handlePrefix + path + ":edit");
+  }
+}
 
-
-
-    /* window.open(defaultBaseURL +
-      "/.magnolia/admincentral#app:pages:detail;" +
-      handlePrefix +
-      pathname.replace("." + defaultExtension, "") +
-      ":edit"); */
-})();
+// Create a parent item and two children.
+var menu = chrome.contextMenus.create({title: "Edit in Magnolia", onclick: editInMagnolia});
