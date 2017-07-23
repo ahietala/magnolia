a3d014a99f158e4942089cb3b70a47c9cfc0ec2f
ahietala <antti.hietala@gmail.com>
2017-07-23 21:58:47 +0200

    Working Chrome extension

--- edit-in-magnolia/editInMagnolia.user.js
+++ edit-in-magnolia/editInMagnolia.user.js
@@ -10,8 +10,8 @@
 
 
 function editInMagnolia() {
-
   'use strict';
+
   // Author instance and site configuration
   var defaultBaseURL = "http://demoauthor.magnolia-cms.com";
   var defaultExtension = "html";
@@ -22,45 +22,40 @@ function editInMagnolia() {
   var apps = {
     "/tours":"tours",
   };
-  console.log(apps);
 
-  // Doesn't work!!!! argh
-  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    const url = new URL(tabs[0].url);
-    console.log(url.hostname); // "www.example.com"
-    console.log(url.pathname); // "/cats"
+  // Get the current URL
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+    var url = new URL(tabs[0].url);
     path = url.pathname;
+
+    // Remove selectors and extension from the path.
+    if (path.includes("~")) {
+      path = path.replace(/~.*/, "");
+    }
+    else {
+      path = path.replace(/\.[^/.]+$/, "");
+    }
+
+    // Check if first path segment is a known app prefix.
+    var segments = path.split("/");
+    var firstSegment = "/" + segments[1];
+
+    if (firstSegment in apps) {
+      // Current content is managed in a content app
+      var appName = apps[firstSegment];
+      segments.splice(1,1);
+      var pathInApp = segments.join("/");
+      var newURL = defaultBaseURL + "/.magnolia/admincentral#app:" + appName + ":detail;" + pathInApp + ":edit";
+      chrome.tabs.create({url: newURL});
+    } else {
+      // Current content is managed in the Pages app
+      var newURL = defaultBaseURL + "/.magnolia/admincentral#app:pages:detail;" + handlePrefix + path + ":edit";
+      chrome.tabs.create({url: newURL});
+    }
+
   });
 
-  // Remove selectors and extension from the path.
-  // var path = window.location.pathname;
-  if (path.includes("~")) {
-    path = path.replace(/~.*/, "");
-    console.log("path without selectors: " + path);
-  }
-  else {
-    path = path.replace(/\.[^/.]+$/, "");
-    console.log("path without extension: " + path);
-  }
 
-  // Check if first path segment is a known app prefix.
-
-  var segments = path.split("/");
-  console.log(segments);
-
-  var firstSegment = "/" + segments[1];
-  console.log(firstSegment);
-
-  if (firstSegment in apps) {
-    var appName = apps[firstSegment];
-    segments.splice(1,1);
-    var pathInApp = segments.join("/");
-    console.log("This content is managed in the " + appName + " app.");
-    console.log("App URL: " + defaultBaseURL + "/.magnolia/admincentral#app:" + appName + ":detail;" + pathInApp + ":edit");
-  } else {
-    console.log("This content is managed in the Pages app.");
-    console.log("Page URL: " + defaultBaseURL + "/.magnolia/admincentral#app:pages:detail;" + handlePrefix + path + ":edit");
-  }
 }
 
 // Create a parent item and two children.
